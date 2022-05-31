26bccbbb271d19e24e9c06d678e307f4887dccdf
shreydan <iamshreydan@gmail.com>
2022-05-31 10:59:57 +0530

    wrapper file

---
+++ wrapper.py
@@ -0,0 +1,23 @@
+from generator import modify_html, convert_to_image
+from fetcher import fetchVerse
+import os
+import json
+
+month = [f'2022-05-{str(i).zfill(2)}' for i in range(17,18)]
+_month = 'MAY'
+
+f = open(os.path.join(os.getcwd(),'calendar-data-2022.json'))
+dates = json.load(f)
+f.close()
+
+print("Praise the Lord!")
+
+for date in month:
+    verseID = dates['Hebron'][date]['verseid']
+    print(verseID, date)
+    verseObj = fetchVerse(verseID,date)
+    modify_html(verseObj)
+    convert_to_image(verseObj,_month)
+
+
+    print(f"generated {verseObj['book'][0]} {verseObj['reference']}")
