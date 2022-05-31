958c9b6ba46ebf3b8daad11410ee15b03e7ff3fe
shreydan <iamshreydan@gmail.com>
2022-05-31 10:54:06 +0530

    generate calendar

---
+++ calendar_generator.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+from datetime import date, timedelta
+
+
+def main(db_path):
+    days = {}
+    start_date = date(2022,1,1)
+    end_date = date(2022,12,31)
+    delta = end_date - start_date
+    for i in range(delta.days + 1):
+        day = start_date + timedelta(days=i)
+        days.update({day.isoformat(): {
+            "verseid": ""
+        }})
+    calendar_obj = {
+        "hebron": days
+    }
+    with open(db_path,'w') as json_file:
+        json.dump(calendar_obj, json_file, indent=4)
+
+
+
+if __name__ == '__main__':
+    year = '2022'
+    db_path = Path('calendar-data-2022.json')
+    db_path.touch(exist_ok=True)
+    main(db_path)
