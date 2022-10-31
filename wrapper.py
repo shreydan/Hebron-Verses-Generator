5d97a57e57c70d5f34d0fd74f654b1d297a92b86
shreydan <iamshreydan@gmail.com>
2022-10-31 23:28:02 +0530

    november update

--- wrapper.py
+++ wrapper.py
@@ -3,8 +3,8 @@ from fetcher import fetchVerse
 import os
 import json
 
-month = [f'2022-10-{str(i).zfill(2)}' for i in range(1,32)]
-_month = 'october'
+month = [f'2022-11-{str(i).zfill(2)}' for i in range(1,31)]
+_month = 'november'
 
 f = open(os.path.join(os.getcwd(),'calendar-data-2022.json'))
 dates = json.load(f)
