29ff61717e044a226492e0eb7e17ce429a355b00
shreydan <iamshreydan@gmail.com>
2023-03-31 23:55:06 +0530

    APRIL update :)

--- wrapper.py
+++ wrapper.py
@@ -3,8 +3,8 @@ from fetcher import fetchVerse
 import os
 import json
 
-month = [f'2023-03-{str(i).zfill(2)}' for i in range(1,32)]
-_month = 'MARCH'
+month = [f'2023-04-{str(i).zfill(2)}' for i in range(1,31)]
+_month = 'APRIL'
 
 f = open(os.path.join(os.getcwd(),'calendar-data-2023.json'))
 dates = json.load(f)
