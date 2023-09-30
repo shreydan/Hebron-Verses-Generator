c7ad6576dd5eaf6756552206640ae1e0403b130c
shreydan <iamshreydan@gmail.com>
2023-10-01 00:48:32 +0530

    october

--- wrapper.py
+++ wrapper.py
@@ -4,9 +4,9 @@ import os
 import json
 import time
 
-month = [f'2023-09-{str(i).zfill(2)}' for i in range(1,31)]
+month = [f'2023-10-{str(i).zfill(2)}' for i in range(1,32)]
 
-_month = 'SEPTEMBER'
+_month = 'OCTOBER'
 
 f = open(os.path.join(os.getcwd(),'calendar-data-2023.json'))
 dates = json.load(f)
